63c8ed637ebb97af5873cb875dc3cbec3456123f
Deep Chatterjee <deep.chatterjee@ligo.org>
2021-03-29 16:05:35 -0500

    add O5 stacked figure and scripts

--- scripts/stacked-h0-O5.py
+++ scripts/stacked-h0-O5.py
@@ -31,30 +31,29 @@ def add_hubble(result):
     hubble_vals /= result.posterior.luminosity_distance.values
     hubble_vals *= (1 + result.posterior.z.values)
     hubble_vals *= (const.c.to('km/s')/u.Mpc).value
-    result.posterior['hubble'] = hubble_vals
+    result.posterior['h0'] = hubble_vals
 
 
 def add_reweighted_hubble(row, num_prior_points=10000,
                           bin_start=-10, bin_step=20,
-                          num_bins=15, plot=True):
+                          num_bins=15, plot=False):
     result = row.bilby_result
-    hubble_posterior = result.posterior.hubble.values
-
-    z_prior = result.priors['z'].sample(num_prior_points)
-    distance_prior = result.priors['luminosity_distance'].sample(num_prior_points)
-
-    hubble_prior = np.array([quad(integrand, 0, z_vals)[0] for z_vals in z_prior])
-
-    hubble_prior /= distance_prior
-    hubble_prior *= (1 + z_prior)
-    hubble_prior *= (const.c.to('km/s')/u.Mpc).value
+    hubble_posterior = result.posterior.h0.values
+    try:
+        hubble_prior = result.priors['h0'].sample(num_prior_points)
+    except KeyError:
+        z_prior = result.priors['z'].sample(num_prior_points)
+        distance_prior = result.priors['luminosity_distance'].sample(num_prior_points)
+        hubble_prior = np.array([quad(integrand, 0, z_vals)[0] for z_vals in z_prior])
+        hubble_prior /= distance_prior
+        hubble_prior *= (1 + z_prior)
+        hubble_prior *= (const.c.to('km/s')/u.Mpc).value
 
     # cut the prior at max value of the bin
     bins = [bin_start + n*bin_step for n in range(num_bins)]
     accept_mask = hubble_prior < (max(bins) - bin_step)
 
-    z_prior = z_prior[accept_mask]
-    distance_prior = distance_prior[accept_mask]
+    #distance_prior = distance_prior[accept_mask]
     hubble_prior = hubble_prior[accept_mask]
     # cut posterior values at 99 percentile
     hubble_posterior = hubble_posterior[
@@ -74,15 +73,15 @@ def add_reweighted_hubble(row, num_prior_points=10000,
     wts = 1./weights_interpolant(hubble_prior)
     wts /= np.sum(wts)
 
-    # print(hubble_prior[np.where(np.isnan(wts))])
-    # print(hubble_prior[np.where(np.isinf(wts))])
+    #print(hubble_prior[np.where(np.isnan(wts))])
+    #print(hubble_prior[np.where(np.isinf(wts))])
 
     posterior_wts = 1./weights_interpolant(hubble_posterior)
     posterior_wts /= np.sum(posterior_wts)
 
-    # print("Bad posterior values")
-    # print(hubble_posterior[np.where(np.isnan(posterior_wts))])
-    # print(hubble_posterior[np.where(np.isinf(posterior_wts))])
+    #print("Bad posterior values")
+    #print(hubble_posterior[np.where(np.isnan(posterior_wts))])
+    #print(hubble_posterior[np.where(np.isinf(posterior_wts))])
 
     if plot:
         _, _b, _p = plt.hist(hubble_posterior, weights=posterior_wts,
@@ -96,14 +95,15 @@ def add_reweighted_hubble(row, num_prior_points=10000,
         fiducial_hubble_vals = np.linspace(0, 200, 100)
         reweighted_kde_hubble_vals = reweighted_kde(fiducial_hubble_vals)
         plt.plot(fiducial_hubble_vals, reweighted_kde_hubble_vals, label='KDE')
+        plt.title(f"Inclination: {row.inclination:.1f}, Distance: {row.distance}")
         plt.legend()
         plt.show()
     return hubble_posterior, posterior_wts
 
 
 filenames = glob.glob(
-    '/home/deep/work/campus-cluster-runs/pbilby-runs/redshift-injections/ce/'
-    'outdir_gw_tidal_cosmo_dl_[1234567]000_CE_incl_[13467][0|5]/result/*.json'
+    '/home/deep/work/campus-cluster-runs/pbilby-runs/redshift-injections/O5/'
+    'outdir_gw_tidal_cosmo_dl_[1234567]00_CE_incl_[13467][0|5]/result/*.json'
 )
 filenames = sorted(filenames)
 
@@ -118,11 +118,12 @@ results_master.distance = results_master.distance.astype(float)
 results_master.inclination = results_master.inclination.astype(float)
 
 for idx, r in results_master.iterrows():
-    add_hubble(r.bilby_result)
+    if hasattr(r.bilby_result.posterior, 'z'):
+        add_hubble(r.bilby_result)
 
 # attach posterior for hubble
 results_master['hubble_kde'] = [
-    gaussian_kde(r.bilby_result.posterior.hubble.values)
+    gaussian_kde(r.bilby_result.posterior.h0.values)
     for idx, r in results_master.iterrows()
 ]
 # attach re-weighted hubble
@@ -145,7 +146,7 @@ results_master['reweighted_hubble_kde'] = [
     for idx, r in results_master.iterrows()
 ]
 # load population weights
-with open('/home/deep/github/tidal-cosmology-selections/dl-iota-kde.pickle', 'rb') as f:
+with open('/home/deep/github/gw-tidal-cosmology-docs/data/O5/dl-iota-kde.pickle-O5', 'rb') as f:
     kde = pickle.load(f)
 
 pop_wts = [kde([r.distance, r.inclination/180*np.pi])[0] for idx, r in results_master.iterrows()]
@@ -169,27 +170,7 @@ prior_wt = prior_wts_dist*prior_wts_incl
 prior_wt /= prior_wt.sum()
 results_master['prior_wt'] = prior_wt
 
-# dist_vals = np.linspace(100, 8000, 100)
-# incl_vals = np.linspace(0, np.pi, 20)
-
-# X, Y = np.meshgrid(dist_vals, incl_vals)
-# positions = np.vstack([X.ravel(), Y.ravel()])
-# Z = np.reshape(kde(positions).T, X.shape)
-
-# plt.figure(figsize=(12, 8))
-# plt.contourf(X, Y, Z, cmap='bone')
-# plt.colorbar()
-# plt.xlabel('Distance')
-# plt.ylabel('Inclination')
-# plt.ylim((0, 1.57))
-# plt.scatter(
-#     results_master.distance, results_master.inclination/180*np.pi,
-#     s=50,
-#     c='r',
-#     #c=results_master.prior_wt, cmap='hot'
-# )
-
-relative_pop_wts = results_master.pop_wt/results_master.pop_wt.min()/5
+relative_pop_wts = 10*(results_master.pop_wt/np.sum(results_master.pop_wt))
 
 # Stacked H0
 stacked_h0 = lambda x: reduce(
@@ -200,18 +181,16 @@ stacked_h0 = lambda x: reduce(
                                        relative_pop_wts)
     ]
 )
-norm = quad(stacked_h0, 0, 500)[0]
+norm = quad(stacked_h0, 0, 300)[0]
 norm_stacked_h0 = lambda x: stacked_h0(x)/norm
 
 plt.figure()
 h0_vals = np.linspace(20, 150, 150)
-colors = {'15.0': 'y', '20.0':'y', '30.0': 'y', '37.5': 'y', '45.0': 'm', '60.0': 'm', '75.0': 'm'}
 
 for idx, r in results_master.iterrows():
     plt.plot(h0_vals, r.reweighted_hubble_kde(h0_vals),
              label=f'DL = {r.distance:.1e}; iota = {r.inclination:.1e}',
              color='black',
-             #  color=colors[str(r.inclination)],
              linestyle='dotted')
 
 
@@ -220,4 +199,5 @@ plt.xlim((20, 150))
 plt.axvline(x=70, c='r')
 plt.xlabel('$H_0$ (km s$^{-1}/$Mpc)')
 plt.ylabel('$p(H_0)$')
-plt.savefig('../figures/stacked-h0-ce-v2.pdf')
+#plt.show()
+plt.savefig('../figures/stacked-h0-O5-v1.pdf')
